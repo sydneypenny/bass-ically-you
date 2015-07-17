721f1c3897eef065dd0644631734f65bcf9a040d
Anjuli <komakoh@gmail.com>
2015-07-17 14:30:06 +0000

    copy for Bridget

--- application_controller.rb
+++ application_controller.rb
@@ -1,14 +1,49 @@
 require 'bundler'
 Bundler.require
+require_relative './models/model.rb'
 
 class ApplicationController < Sinatra::Base
 
-  get '/start' do
+  get '/' do
 		erb:index
+  end
+  
+  post '/' do
+    erb:survey
   end
   
   get '/survey' do
     erb:survey
   end
+  
+  post '/survey' do
+    erb:portrait
+  end
+
+# end
+
+  post '/portrait' do
+    puts params
+    name = params[:name]
+    @name = name(name)
+    
+    primary_instrument = params[:instrument]
+    @primary_instrument = instrument(primary_instrument)
+    
+    genre_to_play = params[:play]    
+    @genre_to_play = play(genre_to_play)
+    
+    genre_to_listen = params[:listen]    
+    @genre_to_listen = listen(genre_to_listen)
+    
+    favorite_artist = params[:artist]    
+    @favorite_artist = artist(favorite_artist)
+    
+    picture = params[:picture]
+    @picture = picture
+    
+    erb:portrait
+  end
+    
 
 end
---
+++ models/model.rb
@@ -0,0 +1,19 @@
+def name(name)
+  return name.downcase.capitalize 
+end
+
+def instrument(instrument)
+  return instrument.downcase.capitalize
+end
+
+def play(genrel)
+  return genrel.downcase.capitalize
+end
+
+def listen(genrez)
+  return genrez.downcase.capitalize
+end
+
+def artist(artist)
+  return artist.downcase.capitalize
+end
