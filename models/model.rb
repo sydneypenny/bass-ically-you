2652286d61811ae07b240a20037d7c6daad63041
Sydney Penny <skpenny21@gmail.com>
2015-07-17 17:09:15 +0000

    final?

--- models/model.rb
+++ models/model.rb
@@ -1,3 +1,19 @@
 def name(name)
-  return name 
+  return name.downcase.capitalize 
+end
+
+def instrument(instrument)
+  return instrument.downcase
+end
+
+def play(genrel)
+  return genrel.downcase.capitalize
+end
+
+def listen(genrez)
+  return genrez.downcase.capitalize
+end
+
+def artist(artist)
+  return artist.downcase.capitalize
 end
